c579a9f164b82d05b2f13a7cd2aaa3dde977b783
ea0723 <elenanewton@icloud.com>
2013-04-09 14:11:21 +0800

    initial Hartl commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'ca4be9492ae464242a2a3abe6b33664881a43d5134c9289d4f6c570325a79001285d33f90bf58417ed5ee8b583939858dec6e77e774d5b637beea7e27f04ccd6'
